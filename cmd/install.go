e563231d45cb21351327f8af3f9663a5656c92fc
sokumon <sohamkulkarns9@gmail.com>
2024-08-28 10:27:32 +0530

    Installation works

--- cmd/install.go
+++ cmd/install.go
@@ -4,6 +4,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"wgm/utils"
 )
 
 
@@ -11,6 +12,10 @@ var installCmd = &cobra.Command{
 	Use:   "install",
 	Short: "Creates a client.conf for the server",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Installing Wireguard")
+		fmt.Print("Installing Wireguard")
+		if utils.CheckOS() == "Ubuntu" || utils.CheckOS() == "Debian" {
+			utils.ExecuteCommand("sudo","apt","update")
+			utils.ExecuteCommand("sudo","apt","install","wireguard","-y")
+		}
 	},
 }
--- utils/runcmd.go
+++ utils/runcmd.go
@@ -3,18 +3,35 @@ package utils
 import (
 	"os/exec"
 	"fmt"
+    "strings"
 )
 
-func ExecuteCommand(app string, args ...string) {
+func ExecuteCommand(app string, args ...string) string {
     // Create a new command using the variadic args
     cmd := exec.Command(app, args...)
     stdout, err := cmd.Output()
 
     if err != nil {
         fmt.Println(err.Error())
-        return
+        return err.Error()
     }
 
     // Print the output
-    fmt.Println(string(stdout))
+    return string(stdout)
+}
+
+
+func CheckOS() string {
+    output := ExecuteCommand("cat","/etc/os-release")
+    temp := strings.Split(output,"\n")
+    var osName string
+    for _, line := range temp {
+        if strings.HasPrefix(line, "NAME=") {
+            osName = strings.TrimPrefix(line, "NAME=")
+            osName = strings.Trim(osName, `"`) // Remove the double quotes
+            break
+        }
+    }
+
+    return osName
 }
